41c1c25f05fc3ac936bfbb55497985b48f8566f3
red-eyed-coder-club <red.eyed.coder.club@gmail.com>
2021-07-05 13:11:42 +0300

    Delete gh-pages dir-push

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,10 +5,6 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  router: {
-    base: '/red-eyed-coder-club.github.io/'
-  },
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Red Eyed Coder Club',
