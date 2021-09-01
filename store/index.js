1f5e3baf5d5885a53ab3cb80f937fb240822af84
red-eyed-coder-club <red.eyed.coder.club@gmail.com>
2021-09-01 18:25:20 +0300

    Add Flask and Vue.js source code

--- store/index.js
+++ store/index.js
@@ -8,109 +8,115 @@ export const state = () => ({
       "url": "/source-code",
       "items": [
         {
-          "id": 1,
+          "id": 19,
+          "title": "Python Flask and Vue.js tutorial - TODO app project",
+          "code": "https://www.patreon.com/posts/flask-2-vue-js-54892532",
+          "video": "https://youtu.be/_YeN69XoqqU"
+        },
+        {
+          "id": 18,
           "title": "Django Channels + Vue.js + Celery + Redis: CoinMarketCap Clone",
           "code": "https://www.patreon.com/posts/django-channels-46279038",
           "video": "https://youtu.be/wos1uhnd3qM"
         },
         {
-          "id": 2,
+          "id": 17,
           "title": "Django Channels Real Time Graph",
           "code": "https://www.patreon.com/posts/django-channels-45356633",
           "video": "https://youtu.be/tZY260UyAiE"
         },
         {
-          "id": 3,
+          "id": 16,
           "title": "Most minimal Django Channels example",
           "code": "https://www.patreon.com/posts/most-minimal-45284762",
           "video": "https://youtu.be/R4-XRK6NqMA"
         },
         {
-          "id": 4,
+          "id": 15,
           "title": "Blockchain with Python",
           "code": "https://www.patreon.com/posts/blockchain-with-44951681",
           "video": "https://www.youtube.com/watch?v=2KjqfObP4_0&list=PLe4mIUXfbIqYlbm6tNK9NExxgACsoq5e5"
         },
         {
-          "id": 5,
+          "id": 14,
           "title": "How to scrape Amazon.com with Python, Selenium and BeautifulSoup",
           "code": "https://www.patreon.com/posts/how-to-scrape-43957077",
           "video": "https://youtu.be/497Fy7CIBOk"
         },
         {
-          "id": 6,
+          "id": 13,
           "title": "Flask Blog Engine project",
           "code": "https://www.patreon.com/posts/flask-blog-code-38844531",
           "video": "https://www.youtube.com/playlist?list=PLe4mIUXfbIqaLWrzsSDQAAK3_NQB1jBZZ"
         },
         {
-          "id": 7,
+          "id": 12,
           "title": "Django REST + Vue.js tutorial",
           "code": "https://www.patreon.com/posts/django-rest-vue-37438194",
           "video": "https://www.youtube.com/watch?v=7GWKv03Osek"
         },
         {
-          "id": 8,
+          "id": 11,
           "title": "Django 3 TODO app with Vue.js",
           "code": "https://www.patreon.com/posts/django-3-todo-js-36737726",
           "video": "https://www.youtube.com/watch?v=xq532yn8gMA"
         },
         {
-          "id": 9,
+          "id": 10,
           "title": "Steam Store scraping with Python and Requests",
           "code": "https://www.patreon.com/posts/steam-store-with-35670113",
           "video": "https://youtu.be/3fcKKZMFbyA"
         },
         {
-          "id": 10,
+          "id": 9,
           "title": "How to get CSRF Tokens from Cookies in Django",
           "code": "https://www.patreon.com/posts/how-to-get-csrf-35246639",
           "video": "https://youtu.be/Zx0TbDDidsw"
         },
         {
-          "id": 11,
+          "id": 8,
           "title": "Django Casts #1: Basic AJAX",
           "code": "https://www.patreon.com/posts/django-casts-1-35081961",
           "video": "https://youtu.be/QDdLvImfq_g"
         },
         {
-          "id": 12,
+          "id": 7,
           "title": "Django TODO app with Ajax and Jquery",
           "code": "https://www.patreon.com/posts/django-todo-app-34786355",
           "video": "https://youtu.be/zxvtUr3Cmto"
         },
         {
-          "id": 13,
+          "id": 6,
           "title": "Django 3 Tutorial Blog Engine",
           "code": "https://www.patreon.com/posts/django-3-blog-34240101",
           "video": "https://www.youtube.com/playlist?list=PLe4mIUXfbIqbbqgDSY6z9orgzIwvYZjtw"
         },
         {
-          "id": 14,
+          "id": 5,
           "title": "Telegram bot for Coinmarketcap website",
           "code": "https://www.patreon.com/posts/telegram-bot-for-27990251",
           "video": "https://www.youtube.com/watch?v=cX8m3sp_w84&list=PLe4mIUXfbIqbpQkE2Q_iQic6a8v-2k_m4"
         },
         {
-          "id": 15,
+          "id": 4,
           "title": "The Web GUI for the log parser with Flask",
           "code": "https://www.patreon.com/posts/web-gui-for-log-27990225",
           "video": "https://youtu.be/4v3NEGfeff4"
         },
         {
-          "id": 16,
+          "id": 3,
           "title": "The Ebay scraper project",
           "code": "https://www.patreon.com/posts/ebay-scraper-27990166",
           "video": "https://youtu.be/m4hEAhHHykI"
         },
         {
-          "id": 17,
+          "id": 2,
           "title": "The Apache logs parser project",
           "code": "https://www.patreon.com/posts/apache-logs-code-27990114",
           "video": "https://www.youtube.com/watch?v=4DdueeIE8Rs"
         },
         {
-          "id": 18,
+          "id": 1,
           "title": "Kivy - the Coppa project",
           "code": "https://www.patreon.com/posts/kivy-coppa-code-27970971",
           "video": "https://www.youtube.com/playlist?list=PLe4mIUXfbIqbeT-2SxDy_IbQYU4yn2gA6"
